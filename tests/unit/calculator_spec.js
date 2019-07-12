21156f6cf18a693193b8d49ffdab351aa77fa855
Robert Tam <rsh.tam@gmail.com>
2019-07-12 10:59:05 +0100

    Add all unit tests

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -15,18 +15,28 @@ describe('calculator', function () {
   it('should add 1 to 4 and get 5', function() {
     calculator.previousTotal = 4;
     calculator.add(1);
-    assert.strictEqual(calculator.runningTotal, 5)
+    assert.strictEqual(calculator.runningTotal, 5);
   });
 
   //subtract 4 from 7 and get 3
   it('should subtract 4 from 7 and get 3', function() {
     calculator.previousTotal = 7;
     calculator.subtract(4);
-    assert.strictEqual(calculator.runningTotal, 3)
+    assert.strictEqual(calculator.runningTotal, 3);
   });
 
   // multiply 3 by 5 and get 15
+  it('should multiply 3 by 5 and get 15', function() {
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    assert.strictEqual(calculator.runningTotal, 15);
+  });
 
   // divide 21 by 7 and get 3
+  it('should divide 21 by 7 and get 3', function() {
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    assert.strictEqual(calculator.runningTotal, 3);
+  });
 
 });
