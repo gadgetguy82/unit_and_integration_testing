eb5d3de2e8061fd9586980cf2b7b31a1f47d3037
Robert Tam <rsh.tam@gmail.com>
2019-07-12 10:28:39 +0100

    Add files to repo

---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,25 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  });
+
+  // add 1 to 4 and get 5
+  it('should add 1 to 4 and get 5', function() {
+
+  });
+
+  //subtract 4 from 7 and get 3
+
+  // multiply 3 by 5 and get 15
+
+  // divide 21 by 7 and get 3
+
+});
