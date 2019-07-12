9c8a888a2c6c432be0da81497b2e1039d2255964
Robert Tam <rsh.tam@gmail.com>
2019-07-12 10:55:17 +0100

    Add tests for add and subtract

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -13,10 +13,17 @@ describe('calculator', function () {
 
   // add 1 to 4 and get 5
   it('should add 1 to 4 and get 5', function() {
-
+    calculator.previousTotal = 4;
+    calculator.add(1);
+    assert.strictEqual(calculator.runningTotal, 5)
   });
 
   //subtract 4 from 7 and get 3
+  it('should subtract 4 from 7 and get 3', function() {
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    assert.strictEqual(calculator.runningTotal, 3)
+  });
 
   // multiply 3 by 5 and get 15
 
